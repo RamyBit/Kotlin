04685c93c746fbd570cbe6d4662797fd41ef2185
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-18 13:03:09 +0100

    ControllersTests added

---
+++ blog/src/test/kotlin/com/example/blog/HttpControllersTests.kt
@@ -0,0 +1,35 @@
+package com.example.blog
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest
+class HttpControllersTests(@Autowired val mockMvc: MockMvc) {
+
+    @MockkBean
+    lateinit var userRepository: UserRepository
+
+    @MockkBean
+    lateinit var articleRepository: ArticleRepository
+
+    @Test
+    fun `List articles`(){
+        val johnDoe = User("JohnDoe", "John", "Doe")
+        val loremArticle = Article("Lorem", "Lorem", "dolor sit amet", johnDoe)
+        val ipsumArticle = Article("Ipsum", "Ipsum", "dolor sit amet", johnDoe)
+
+        every { articleRepository.findAllByOrderByAddedAtDesc() } returns listOf(loremArticle,ipsumArticle)
+        mockMvc.perform(get("/api/article/").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("\$.[0].login").value(johnDoe.login))
+            .andExpect(jsonPath("\$.[1].login").value(johnDoe.login))
+    }
+}
