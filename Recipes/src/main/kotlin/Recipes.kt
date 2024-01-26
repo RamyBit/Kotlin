cbb782e5590225c4af2d8bc2783f18c79ceed091
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-26 11:57:04 +0100

    Overloading Example added

--- Recipes/src/main/kotlin/Recipes.kt
+++ Recipes/src/main/kotlin/Recipes.kt
@@ -1,5 +1,5 @@
 
-data class Recipes(val title: String,
+data class Recipe(val title: String,
                    val mainIngredient: String,
                    val isVegetarian: Boolean = false,
                    val difficulty: String = "Easy")
@@ -11,9 +11,23 @@ class Mushroom(val size: Int, val isMagic: Boolean){
     }
 }
 
+fun findRecipes(title: String = "",
+                ingredient: String,
+                isVegetarian: Boolean = false,
+                difficulty: String =""): Array<Recipe>{
+    // TODO code to find recipes
+    return arrayOf(Recipe(title, ingredient,isVegetarian,difficulty))
+}
+// This is an example of a overloaded function in kotlin
+fun addNumbers(a: Int, b: Int) : Int{
+    return a + b
+}
+fun addNumbers(a: Double, b: Double): Double{
+    return a + b
+}
 fun main(args: Array<String>){
-    val r1 = Recipes("Gulasch", false)
-    val r2 = Recipes("Gulasch", false)
+    val r1 = Recipe("Gulasch", false)
+    val r2 = Recipe("Gulasch", false)
     val r3 = r1.copy(title = "Pizza")
     println("r1 hash code: ${r1.hashCode()}")
     println("r2 hash code: ${r2.hashCode()}")
