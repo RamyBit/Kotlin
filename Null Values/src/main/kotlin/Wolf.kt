d7ec468f92fcae042d7a201c13a5e44cd7890382
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-29 10:45:46 +0100

    Null Values Example working state

--- Null Values/src/main/kotlin/Wolf.kt
+++ Null Values/src/main/kotlin/Wolf.kt
@@ -1,3 +1,5 @@
+import com.sun.tools.javac.Main
+
 class Wolf {
     var hunger  = 10
     val food = "Meat"
@@ -17,4 +19,29 @@ class MyWolf{
 
 fun getAlphaWolf(): Wolf? {
     return Wolf()
+}
+
+fun main(args: Array<String>){
+    var w: Wolf? = Wolf()
+    if(w != null){
+        w.eat()
+    }
+    var x = w?.hunger
+    println("The value of x is $x")
+
+    var y = w?.hunger ?: -1
+    println("The value of y is $y")
+
+    var myWolf = MyWolf()
+    myWolf?.wolf?.hunger = 8
+    println("The value of myWolf.wolf?.hunger is ${myWolf?.wolf?.hunger}")
+
+    var myArray = arrayOf("Hello", "World", null)
+    for (item in myArray){
+        item?.let { println(it) }
+    }
+
+    getAlphaWolf()?.let { it.eat() }
+    w = null
+    //var z = w!!.hunger
 }
